6bae13ae01f8499e2bd608ae32bc4362a9fc13f9
Yaroslav Manuylov <Dalahan@mail.ru>
2022-11-11 13:39:27 +0300

    VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,42 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
+import { defineComponent } from 'vue';
 
-// Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      oper: 'sum',
+    };
+  },
+  computed: {
+    result() {
+      return this.calc();
+    },
+  },
+  methods: {
+    calc() {
+      let res = 0;
+      switch (this.oper) {
+        case 'sum':
+          res = this.num1 + this.num2;
+          break;
+        case 'subtract':
+          res = this.num1 - this.num2;
+          break;
+        case 'multiply':
+          res = this.num1 * this.num2;
+          break;
+        case 'divide':
+          res = this.num1 / this.num2;
+          break;
+      }
+      return res;
+    },
+  },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+window.vm = vm;
